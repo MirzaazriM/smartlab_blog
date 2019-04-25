787b81123868d12fd3a42f58100acab54989adae
Mirza O <oglecevacmirza@gmail.com>
2019-04-25 09:25:08 +0200

    Set controller base functions and set routes

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    public function index() {
+        
+    }
+
+    public function show($id) {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BlogController@index');
+Route::get('/blog/{id}', 'BlogController@show');
+
