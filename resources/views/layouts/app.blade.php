c47910352d40bfd410f55165f3d5f3f860945c06
Emir Mekić <emir.mekic@live.com>
2019-05-24 16:04:54 +0200

    removed class "py-4"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,14 +21,13 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <style>
-
         #app {
             width: 90%;
             margin: 0 auto;
         }
-
     </style>
 </head>
+
 <body>
 
     @include('layouts.menu')
@@ -53,10 +53,11 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
     </div>
 
 </body>
+
 </html>
